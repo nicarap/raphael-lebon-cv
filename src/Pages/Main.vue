079283fac9315577f39ed5f1266df7dab60bf101
Raph974 <68837511+nicarap@users.noreply.github.com>
2022-12-17 00:43:04 +0400

    Set presentation and main skin

---
+++ src/Pages/Main.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Header from '../components/Header.vue';
+import Presentation from '../components/Presentation.vue';
+import Card from '../components/Card.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center w-full scroll-smooth">
+        <section class="max-w-sm sm:max-w-2xl h-screen flex flex-col justify-center items-center">
+            <h1 class="font-mono text-4xl text-center"><strong>Développeur Frontend & Backend</strong></h1>
+            <h2 class="font-mono text-2xl mb-12"><a href="#suite" >- Raphaël LEBON -</a></h2>
+            <div class="m-12">
+                <div class="flex justify-center">
+                    <img src="/images/20220503_102526.jpg" class="w-[150px] h-auto border-radius rounded-full" alt="photo"/>
+                </div>
+            </div>
+        </section>
+        <section class="w-full" id="suite">
+            <div class="bg-cyan-400 h-2/3 p-2 pb-56 flex justify-center">
+                <div class="max-w-2xl p-12">
+                    <Presentation id="presentation" />
+                </div>
+            </div>
+            <div class="bg--200 p-2 flex justify-center">
+                <div class="max-w-7xl">
+                    <Card class=" -mt-24"/>
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,46 @@
+<script setup>
+</script>
+
+<template>
+    <div class="bg-white border border-gray-200 rounded-lg shadow-md">
+        <div class="flex">
+            <div class=" p-6 border border-y-0 border-l-0 border-r-1">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-900 text-center">Backend</h5>
+                <p class="mb-3 font-normal text-gray-700">
+                    I value simple content structure, clean design patterns, and thoughtful interactions.
+                </p>
+                <p class="mb-3 font-normal text-red-700">
+                    Languages
+                </p>
+                <p class="mb-3 font-normal text-gray-700">
+                    PHP
+                </p>
+                <p class="mb-3 font-normal text-red-700">
+                    Dev Tools
+                </p>
+                <p class="mb-3 font-normal text-gray-700">
+                    GitLab, GitHub, Laravel
+                </p>
+            </div>
+            <div class=" p-6">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-red-900 text-center">Backend</h5>
+                <p class="mb-3 font-normal text-gray-700">
+                    I value simple content structure, clean design patterns, and thoughtful interactions.
+                </p>
+                <p class="mb-3 font-normal text-red-700">
+                    Languages
+                </p>
+                <p class="mb-3 font-normal text-gray-700">
+                    PHP
+                </p>
+                <p class="mb-3 font-normal text-red-700">
+                    Dev Tools
+                </p>
+                <p class="mb-3 font-normal text-gray-700">
+                    GitLab, GitHub, Laravel
+                </p>
+            </div>
+        </div>
+    </div>
+
+</template>
