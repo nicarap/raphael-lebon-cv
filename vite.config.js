caf7d5bbe364e026dc6c0435bad6e30fb0131cf5
Raph974 <68837511+nicarap@users.noreply.github.com>
2024-03-13 11:36:06 +0400

    Change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/raphael-lebon-cv.github.io/',
+  base: '/raphael-lebon.github.io/',
   plugins: [vue()],
 })
